4708d11428028d2ec0a975888cfda496863abc06
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2017-05-22 22:19:54 -0300

    Refactored rectangle

---
+++ app/src/main/java/com/example/sessostar/brickbreaker/Quadrilateral.java
@@ -0,0 +1,60 @@
+package com.example.sessostar.brickbreaker;
+
+import android.opengl.GLES20;
+import android.opengl.Matrix;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+/**
+ * Implements a Rectangular object
+ */
+
+public class Quadrilateral {
+    private FloatBuffer vertexBuffer;
+
+    private int COORDS_PER_VERTEX = 3;
+    float color[] = {1.f, 0.31372549f, 0.31372549f, 1.0f};
+
+    ShaderHandler shaderHandler;
+
+    /**
+     * @param vertice is an array of Position and should be in counter-clockwise order
+     * @param sh is the shader handler to this object
+     */
+    public Quadrilateral(Position[] vertice, ShaderHandler sh) {
+        shaderHandler = sh;
+
+        int idx = 0;
+        float[] rectCoords = new float[3 * (4 + 1)];
+        rectCoords[idx++] = vertice[1].x;
+        rectCoords[idx++] = vertice[1].y;
+        rectCoords[idx++] = 0.0f;
+        rectCoords[idx++] = vertice[0].x;
+        rectCoords[idx++] = vertice[0].y;
+        rectCoords[idx++] = 0.0f;
+        rectCoords[idx++] = vertice[2].x;
+        rectCoords[idx++] = vertice[2].y;
+        rectCoords[idx++] = 0.0f;
+        rectCoords[idx++] = vertice[3].x;
+        rectCoords[idx++] = vertice[3].y;
+        rectCoords[idx++] = 0.0f;
+
+        // 4 bytes per float
+        ByteBuffer bb = ByteBuffer.allocateDirect(rectCoords.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(rectCoords);
+        vertexBuffer.position(0);
+    }
+
+
+    public void draw() {
+        int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+        shaderHandler.setPosition(COORDS_PER_VERTEX, vertexStride, vertexBuffer);
+        shaderHandler.setColor(color);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+    }
+}
--- app/src/main/java/com/example/sessostar/brickbreaker/Rectangle.java
+++ app/src/main/java/com/example/sessostar/brickbreaker/Rectangle.java
@@ -1,70 +1,34 @@
 package com.example.sessostar.brickbreaker;
 
-import android.opengl.GLES20;
 import android.opengl.Matrix;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
-
 /**
- * Implements a Rectangular object
+ * Created by gustavo on 5/22/17.
  */
 
 public class Rectangle {
-    private FloatBuffer vertexBuffer;
 
-    static float centerX = 0;
-    static float centerY = 0;
+    private Quadrilateral quadrilateral;
+    private ShaderHandler shaderHandler;
 
-    private int COORDS_PER_VERTEX = 3;
-    float color[] = {1.f, 0.31372549f, 0.31372549f, 1.0f};
-
-    ShaderHandler shaderHandler;
-
-    /**
-     * @param width Rectangle width
-     * @param height Rectangle height
-     */
     public Rectangle(float width, float height, ShaderHandler sh) {
         shaderHandler = sh;
 
-        int idx = 0;
-        float[] rectCoords = new float[3 * (4 + 1)];
-        rectCoords[idx++] = centerX + (-1) * (width / 2);
-        rectCoords[idx++] = centerY + (-1) * (height / 2);
-        rectCoords[idx++] = 0.0f;
-        rectCoords[idx++] = centerX + (-1) * (width / 2);
-        rectCoords[idx++] = centerY + ( 1) * (height / 2);
-        rectCoords[idx++] = 0.0f;
-        rectCoords[idx++] = centerX + ( 1) * (width / 2);
-        rectCoords[idx++] = centerY + (-1) * (height / 2);
-        rectCoords[idx++] = 0.0f;
-        rectCoords[idx++] = centerX + ( 1) * (width / 2);
-        rectCoords[idx++] = centerY + ( 1) * (height / 2);
-        rectCoords[idx++] = 0.0f;
-
-        // 4 bytes per float
-        ByteBuffer bb = ByteBuffer.allocateDirect(rectCoords.length * 4);
-        bb.order(ByteOrder.nativeOrder());
-
-        vertexBuffer = bb.asFloatBuffer();
-        vertexBuffer.put(rectCoords);
-        vertexBuffer.position(0);
+        Position[] rectVertice = new Position[4];
+        rectVertice[0] = new Position((-1) * (width / 2), (-1) * (height / 2));
+        rectVertice[1] = new Position(( 1) * (width / 2), (-1) * (height / 2));
+        rectVertice[2] = new Position(( 1) * (width / 2), ( 1) * (height / 2));
+        rectVertice[3] = new Position((-1) * (width / 2), ( 1) * (height / 2));;
+        quadrilateral = new Quadrilateral(rectVertice, sh);
     }
 
 
     public void draw() {
         float[] translationMatrix = new float[16];
-
         Matrix.setIdentityM(translationMatrix, 0);
         Matrix.translateM(translationMatrix, 0, 0, 0, 0);
         shaderHandler.setModelMatrix(translationMatrix);
 
-
-        int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
-        shaderHandler.setPosition(COORDS_PER_VERTEX, vertexStride, vertexBuffer);
-        shaderHandler.setColor(color);
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+        quadrilateral.draw();
     }
 }
